5f53c5fff8a1bf31b1a127994c4b6d44d5dc6aea
mhlbocian <bocian.michal@outlook.com>
2025-04-24 19:24:07 +0200

    CWF-PHP Example application =========================== This is an initial commit of almost-ready framework reference application. CWF-PHP is now composer-ready

---
+++ src/Public/index.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * CWF-PHP Example Application
+ * 
+ * File: src/Public/index.php
+ * Description: Main index file
+ * Author: Michal Bocian <bocian.michal@outlook.com>
+ * License: 3-Clause BSD
+ */
+
+require_once '../bootstrap.php';
+
+use Mhlbocian\CwfPhp\Router;
+use Mhlbocian\CwfPhp\Exceptions\Router_Exception;
+use Mhlbocian\CwfPhp\Url;
+
+// TODO: Filter PATH_INFO only for alphanumeric and slash chars
+// [!] NOW IT'S VERY UNSAFE AND USED ONLY FOR EARLY DEVELOPMENT
+
+$router = new Router($_SERVER["PATH_INFO"] ?? null);
+
+try {
+    $router->Execute();
+} catch (Router_Exception $ex) {
+// action for invalid route
+    Url::Redirect();
+}
---
+++ src/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+require_once __DIR__ . "/../vendor/autoload.php";
+
+define("APPDIR", __DIR__);
+
+use Mhlbocian\CwfPhp\Framework\Core;
+
+Mhlbocian\CwfPhp\Core::Setup();
